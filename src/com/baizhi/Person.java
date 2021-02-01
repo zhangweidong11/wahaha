eee8f11c1a0aeee693778f08ded024ba5ccee06a
username <194942941@qq.com>
2021-02-01 17:00:33 +0800

    我写了一个person类

---
+++ src/com/baizhi/Person.java
@@ -0,0 +1,14 @@
+package com.baizhi;
+
+/**
+ * @ClassName User
+ * @Author Administrator
+ * @Date 2021/2/1 14:33
+ * @Description TOOO
+ */
+public class Person {
+
+    private String id;
+    private String name;
+    private String pwd;
+}
